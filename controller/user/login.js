bbb8be9d56ac93b52ae81a436716f11a3aaa2767
zhouquan <zhouquan891213@126.com>
2016-01-11 21:04:45 +0800

    New changelist

--- controller/user/login.js
+++ controller/user/login.js
@@ -4,10 +4,15 @@ module.exports = function(req, res, next){
     if(!userName || !password){
         return next(new Error("用户名和密码不能为空！"));
     }
-    if(userName == "zhouquan" && password == "zhouquan"){
+    if(userName == password){
         console.log("登录成功，登录用户名：" + userName);
-        res.setHeader("Set-Cookie", "sessionId=zhouquan");
-        return next();
+        res.setHeader("Set-Cookie", "sessionId=" + userName);
+        var retObj = {
+            flag : true,
+            message : "登录成功，当前用户：" + userName
+        }
+        res.write(JSON.stringify(retObj));
+        res.end();
     }
     return next(new Error("用户名或密码错误！"));
 }
